00cd81ccc97fd5fef4018e02d4aefadbb7131e3b
MelnikNO <159802733+MelnikNO@users.noreply.github.com>
2025-03-11 18:10:37 +0300

    Add files via upload

---
+++ ЛР 1/bintreerec.py
@@ -0,0 +1,28 @@
+def gen_bin_rec_tree(root, height):
+    tree = {}
+
+    def bin_tree(r, h):
+        if h > 0:
+            tree[r] = []
+            left_leaf = r ** 2
+            right_leaf = r - 2
+
+            tree[r].append(bin_tree(left_leaf, h - 1))
+            tree[r].append(bin_tree(right_leaf, h - 1))
+
+        return r
+
+    if height == 0:
+        return {root: []}
+    bin_tree(root, height)
+    return tree
+
+def main():
+    print("Рекурсивное бинарное дерево:")
+    print(gen_bin_rec_tree(root=5, height=6))
+
+if __name__ == "__main__":
+    main()
+
+
+
